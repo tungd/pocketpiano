5150c00e092b8152d107cfd170ea28cee187014e
QuangHuy Tran <quanghuy@QuangHuys-MacBook-Pro.local>
2016-04-24 00:22:05 +0700

    refactor code

--- app/src/main/java/com/pocketpiano/pocketpiano/instruments/PianoNodes.java
+++ app/src/main/java/com/pocketpiano/pocketpiano/instruments/PianoNodes.java
@@ -1,5 +1,6 @@
 package com.pocketpiano.pocketpiano.instruments;
 
+import android.content.Context;
 import android.media.JetPlayer;
 import com.pocketpiano.pocketpiano.R;
 
@@ -8,248 +9,25 @@ import com.pocketpiano.pocketpiano.R;
  */
 public class PianoNodes {
 
+    Context context;
     JetPlayer jetPlayer = JetPlayer.getJetPlayer();
-    
 
-    public synchronized void A1(){
-        
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a1.mid");
-        
+    public PianoNodes(Context context){
+        this.context = context;
+    }
+    enum Note {
+        A1, A1_, A2, A2_, A3, A3_, A4, A4_,
+        B1, B2, B3, B4,
+        C1, C1_, C2, C2_, C3, C3_, C4, C4_,
+        D1, D1_, D2, D2_, D3, D3_, D4, D4_,
+        E1, E2, E3, E4,
+        F1, F1_, F2, F2_, F3, F3_, F4, F4_,
+        G1, G1_, G2, G2_, G3, G3_, G4, G4_
     }
 
-    public synchronized void A1_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a1_.mid");
-        
-    }
+    public void play(Note note) {
 
-    public synchronized void A2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a2.mid");
-        
-    }
-
-    public synchronized void A2_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a2_.mid");
-        
-    }
-
-    public synchronized void A3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a3.mid");
-        
-    }
-
-    public synchronized void A3_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a3_.mid");
-        
-    }
-
-    public synchronized void A4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a4.mid");
-        
-    }
-
-    public synchronized void A4_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/a4_.mid");
-        
-    }
-
-    public synchronized void G1(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g1.mid");
-        
-    }
-
-    public synchronized void G1_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g1_.mid");
-        
-    }
-
-    public synchronized void G2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g2.mid");
-        
-    }
-
-    public synchronized void G2_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g2_.mid");
-        
-    }
-
-    public synchronized void G3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g3.mid");
-        
-    }
-
-    public synchronized void G3_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g3_.mid");
-        
-    }
-
-    public synchronized void G4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g4.mid");
-        
-    }
-
-    public synchronized void G4_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/g4_.mid");
-        
-    }
-
-    public synchronized void F1(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f1.mid");
-        
-    }
-
-    public synchronized void F1_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f1_.mid");
-        
-    }
-
-    public synchronized void F2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f2.mid");
-        
-    }
-
-    public synchronized void F2_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f2_.mid");
-        
-    }
-
-    public synchronized void F3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f3_.mid");
-        
-    }
-
-    public synchronized void F3_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f3_.mid");
-        
-    }
-
-    public synchronized void F4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f4.mid");
-        
-    }
-
-    public synchronized void F4_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/f4_.mid");
-        
-    }
-
-    public synchronized void E1(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/e1.mid");
-        
-    }
-
-    public synchronized void E2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/e2.mid");
-        
-    }
-
-    public synchronized void E3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/e3.mid");
-        
-    }
-
-    public synchronized void E4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/e4.mid");
-        
-    }
-
-    public synchronized void D1(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d1.mid");
-        
-    }
-
-    public synchronized void D1_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d1_.mid");
-        
-    }
-
-    public synchronized void D2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d2.mid");
-        
-    }
-
-    public synchronized void D2_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d2_.mid");
-        
-    }
-
-    public synchronized void D3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d3.mid");
-        
-    }
-
-    public synchronized void D3_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d3_.mid");
-        
-    }
-
-    public synchronized void D4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d4.mid");
-        
-    }
-
-    public synchronized void D4_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/d4_.mid");
-        
-    }
-
-    public synchronized void C1(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c1.mid");
-        
-    }
-
-    public synchronized void C1_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c1_.mid");
-        
-    }
-
-    public synchronized void C2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c2.mid");
-        
-    }
-
-    public synchronized void C2_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c2_.mid");
-        
-    }
-
-    public synchronized void C3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c3.mid");
-        
-    }
-
-    public synchronized void C3_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c3_.mid");
-        
-    }
-
-    public synchronized void C4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c4.mid");
-        
-    }
-
-    public synchronized void C4_(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/c4_.mid");
-        
-    }
-
-    public synchronized void B1(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/b1.mid");
-        
-    }
-
-    public synchronized void B2(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/b2.mid");
-        
-    }
-
-    public synchronized void B3(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/b3.mid");
-        
-    }
-
-    public synchronized void B4(){
-        jetPlayer.loadJetFile("file:///sdcard/mdi/b4.mid");
-        
+        jetPlayer.loadJetFile(String.format("android.resource://" + context.getPackageName() + "/raw/"+ "/%s.mid", note.name().toLowerCase()));
     }
 
     public void startMedia(){
