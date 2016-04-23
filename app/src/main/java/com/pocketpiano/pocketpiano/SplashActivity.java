fb72012b6d227531fb067d73ec18971bcec2896e
Trinh Duc Duy <duytd.hanu@gmail.com>
2016-04-23 22:05:50 +0700

    Delay splash screen after 3 seconds

--- app/src/main/java/com/pocketpiano/pocketpiano/SplashActivity.java
+++ app/src/main/java/com/pocketpiano/pocketpiano/SplashActivity.java
@@ -10,8 +10,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        finish();
+        try {
+            Thread.sleep(3000);
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
